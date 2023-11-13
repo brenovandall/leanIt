b1d7fe845a92b0604410147295fd42b5734ae8e4
Sidney Evers Zuanazzi <sidneyeverszuanazzi@gmail.com>
2023-11-12 21:31:06 -0300

    Inclusão de senha e confirmar senha no cadastro

--- LeanIt_Sidney/paginacadastroleanit/script.js
+++ LeanIt_Sidney/paginacadastroleanit/script.js
@@ -1,12 +1,12 @@
 function validaCPF() {
     let cpfInput = document.querySelector('.inputCpfCadastro');
-    let cpf = cpfInput.value.replace(/\D/g, ''); 
+    let cpf = cpfInput.value.replace(/\D/g, '');
 
     if (cpf.length === 11) {
         cpf = cpf.substring(0, 3) + '.' + cpf.substring(3, 6) + '.' + cpf.substring(6, 9) + '-' + cpf.substring(9);
         cpfInput.value = cpf;
     } else {
-       alert('CPF inválido');
+        alert('CPF inválido');
     }
 }
 
@@ -24,5 +24,23 @@ function dadosEndereco() {
         )
         .catch((erro) => console.log(erro))
 }
+
+function mostrarSenha() {
+    const input = document.querySelector('.inputSenhaCadastro');
+
+    const button = document.querySelector('.tooglePass');
+
+    if (input.type == "password") {
+        input.type = "text"
+    }
+
+    else {
+        input.type = "password";
+    }
+}
+
+
+
+
 
 
