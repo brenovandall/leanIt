3666e8a61d8ab0e0056e1c3f3e7e3daf2571cba3
brenovandall <vbreno335@gmail.com>
2023-12-04 22:09:10 -0300

    mvc do projeto

---
+++ LeanIt.Web/LeantIt.Web/LeantIt.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace LeantIt.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
