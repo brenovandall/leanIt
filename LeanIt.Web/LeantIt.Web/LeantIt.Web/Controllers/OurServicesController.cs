1b2c817a453db9a5c9ba8a3e83d2ab1065c3577f
brenovandall <vbreno335@gmail.com>
2023-12-05 16:57:50 -0300

    tela de serviços adicionada

---
+++ LeanIt.Web/LeantIt.Web/LeantIt.Web/Controllers/OurServicesController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeantIt.Web.Controllers;
+
+public class OurServicesController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
--- LeanIt.Web/LeantIt.Web/LeantIt.Web/wwwroot/js/site.js
+++ LeanIt.Web/LeantIt.Web/LeantIt.Web/wwwroot/js/site.js
@@ -38,3 +38,37 @@ function mostrarSenha() {
         input.type = "password";
     }
 }
+
+
+
+            // PAGINA DE SERVIÇOS //
+
+
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const carousel = document.querySelector('.carousel');
+    const prevBtn = document.getElementById('prevBtn');
+    const nextBtn = document.getElementById('nextBtn');
+    let counter = 0;
+
+    nextBtn.addEventListener('click', function () {
+        counter++;
+        updateCarousel();
+    });
+
+    prevBtn.addEventListener('click', function () {
+        counter--;
+        updateCarousel();
+    });
+
+    function updateCarousel() {
+        const totalItems = document.querySelectorAll('.carousel-item').length;
+        if (counter > totalItems - 1) {
+            counter = 0; // Reset to the first item
+        } else if (counter < 0) {
+            counter = totalItems - 1; // Go to the last item
+        }
+        carousel.style.transform = `translateX(${-counter * 100}%)`;
+    }
+});
