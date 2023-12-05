c03d5f38e6936e099c4d7419b85292cf4489b66e
brenovandall <vbreno335@gmail.com>
2023-12-05 16:44:53 -0300

    adicionando telas de login e registro

---
+++ LeanIt.Web/LeantIt.Web/LeantIt.Web/Controllers/AccountController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeantIt.Web.Controllers;
+
+//***************************************************************************************//
+//               ESTE É O CONTROLADOR DO REGISTRO DE USUARIOS E LOGIN                    //
+//***************************************************************************************//
+public class AccountController : Controller
+{
+    public IActionResult Login()
+    {
+        return View();
+    }
+
+
+    [HttpGet]
+    public IActionResult Register()
+    {
+        return View();
+    }
+}
--- LeanIt.Web/LeantIt.Web/LeantIt.Web/wwwroot/js/site.js
+++ LeanIt.Web/LeantIt.Web/LeantIt.Web/wwwroot/js/site.js
@@ -1,4 +1,40 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿function validaCPF() {
+    let cpfInput = document.querySelector('.inputCpfCadastro');
+    let cpf = cpfInput.value.replace(/\D/g, '');
 
-// Write your JavaScript code.
+    if (cpf.length === 11) {
+        cpf = cpf.substring(0, 3) + '.' + cpf.substring(3, 6) + '.' + cpf.substring(6, 9) + '-' + cpf.substring(9);
+        cpfInput.value = cpf;
+    } else {
+        alert('CPF inválido');
+    }
+}
+
+function dadosEndereco() {
+    let cep = document.querySelector('.inputCepCadastro').value;
+    fetch(`https://viacep.com.br/ws/${cep}/json/`)
+        .then((dados) => dados.json())
+        .then((response) => {
+            console.log(response)
+            document.querySelector('.inputEnderecoCadastro').value = response.logradouro
+            document.querySelector('.inputBairroCadastro').value = response.bairro
+            document.querySelector('.inputCidadeCadastro').value = response.localidade
+            document.querySelector('#estado').value = response.uf
+        }
+        )
+        .catch((erro) => console.log(erro))
+}
+
+function mostrarSenha() {
+    const input = document.querySelector('.inputSenhaCadastro');
+
+    const button = document.querySelector('.tooglePass');
+
+    if (input.type == "password") {
+        input.type = "text"
+    }
+
+    else if (input.type == "text") {
+        input.type = "password";
+    }
+}
