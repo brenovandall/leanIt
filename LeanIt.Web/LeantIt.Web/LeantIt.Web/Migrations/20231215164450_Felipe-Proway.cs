331a9f31894b5a3bd58bbeeadd997e339d097f0f
felipewschulz <felipewerners@gmail.com>
2023-12-15 14:32:54 -0300

    ajustando botões add carro e de editar

---
+++ LeanIt.Web/LeantIt.Web/LeantIt.Web/Migrations/20231215164450_Felipe-Proway.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeantIt.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class FelipeProway : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
