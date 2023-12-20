cad4b35e8540d06a5e4f4d55ac2b7676f1f22e6a
felipewschulz <felipewerners@gmail.com>
2023-12-20 17:00:14 -0300

    Commit da parte Saiba mais iniciando

---
+++ LeanIt.Web/LeantIt.Web/LeantIt.Web/Controllers/SaibaController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace LeantIt.Web.Controllers
+{
+    public class SaibaController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
