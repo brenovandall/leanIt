3f618947a2ded5bd4c5292fe0f1e4493eec3b908
PHFirmino <pedromestrenoclash123@gmail.com>
2023-12-22 23:43:14 -0300

    Identity

---
+++ LeanIt.Web/LeantIt.Web/LeantIt.Web/Models/AplicacaoUser.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace LeantIt.Web.Models
+{
+    public class AplicacaoUser : IdentityUser
+    {
+        public string CNH { get; set; }
+        public string CPF { get; set; }
+        public string Sexo { get; set; }
+        public string Telefone { get; set; }
+        public string DataNascimento { get; set; }
+    }
+}
+
