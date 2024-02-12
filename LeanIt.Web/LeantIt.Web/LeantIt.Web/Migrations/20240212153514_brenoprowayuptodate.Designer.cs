b560b410a10e7aa9e45306c07ea1b3fc22a7a7dd
brenovandall <vbreno335@gmail.com>
2024-02-12 13:14:13 -0300

    chatbot alterado

--- LeanIt.Web/LeantIt.Web/LeantIt.Web/Migrations/20240212153514_brenoprowayuptodate.Designer.cs
+++ LeanIt.Web/LeantIt.Web/LeantIt.Web/Migrations/20240212153514_brenoprowayuptodate.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using LeantIt.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -10,9 +11,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace LeantIt.Web.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240212153514_brenoprowayuptodate")]
+    partial class brenoprowayuptodate
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ LeanIt.Web/LeantIt.Web/LeantIt.Web/Migrations/20240212153514_brenoprowayuptodate.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace LeantIt.Web.Migrations
+{
+    /// <inheritdoc />
+    public partial class brenoprowayuptodate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "1", "efe62718-aad3-44ae-9316-3c461288414c" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "efe62718-aad3-44ae-9316-3c461288414c");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "CNH", "CPF", "ConcurrencyStamp", "DataNascimento", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "Nome", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "Sexo", "Telefone", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "498f6b09-9f57-48eb-8013-f1a3be855534", 0, "000000000000", "00000000000", "ea8bc0b3-db27-43d0-abce-e749e99a87ec", "2000-01-01", "admin@gmail.com", true, false, null, "admin@gmail.com", "ADMIN@GMAIL.COM", "ADMIN@GMAIL.COM", "AQAAAAIAAYagAAAAEGClhKNFZoZ1FmPLPoJpl859PjtpwZwDbNRT3Ai9g9vw74BBIit1ANx6b8uBFAKsWQ==", "0000000000", false, "727f59f8-8897-40a6-be60-fc8a731778e7", "Masculino", "0000000000", false, "admin@gmail.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "1", "498f6b09-9f57-48eb-8013-f1a3be855534" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetUserRoles",
+                keyColumns: new[] { "RoleId", "UserId" },
+                keyValues: new object[] { "1", "498f6b09-9f57-48eb-8013-f1a3be855534" });
+
+            migrationBuilder.DeleteData(
+                table: "AspNetUsers",
+                keyColumn: "Id",
+                keyValue: "498f6b09-9f57-48eb-8013-f1a3be855534");
+
+            migrationBuilder.InsertData(
+                table: "AspNetUsers",
+                columns: new[] { "Id", "AccessFailedCount", "CNH", "CPF", "ConcurrencyStamp", "DataNascimento", "Email", "EmailConfirmed", "LockoutEnabled", "LockoutEnd", "Nome", "NormalizedEmail", "NormalizedUserName", "PasswordHash", "PhoneNumber", "PhoneNumberConfirmed", "SecurityStamp", "Sexo", "Telefone", "TwoFactorEnabled", "UserName" },
+                values: new object[] { "efe62718-aad3-44ae-9316-3c461288414c", 0, "000000000000", "00000000000", "feedfdc7-46b1-4a00-8a11-d854847885e2", "2000-01-01", "admin@gmail.com", true, false, null, "admin@gmail.com", "ADMIN@GMAIL.COM", "ADMIN@GMAIL.COM", "AQAAAAIAAYagAAAAEGJNpLSHeaXKyk3FFOo8gS6KpBxU6S9Fwp+uLYYFCRFctp5NF8P/JfD/u/tXEDqgww==", "0000000000", false, "9fe25b5e-6b1b-4ad9-bf07-ee258796a258", "Masculino", "0000000000", false, "admin@gmail.com" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetUserRoles",
+                columns: new[] { "RoleId", "UserId" },
+                values: new object[] { "1", "efe62718-aad3-44ae-9316-3c461288414c" });
+        }
+    }
+}
