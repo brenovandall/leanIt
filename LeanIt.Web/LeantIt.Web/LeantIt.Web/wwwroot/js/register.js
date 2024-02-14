fa14522d8fceb78e37dabb43a9f7ee95d4be7b01
PHFirmino <pedromestrenoclash123@gmail.com>
2024-02-14 18:22:57 -0300

    detalhes

--- LeanIt.Web/LeantIt.Web/LeantIt.Web/wwwroot/js/register.js
+++ LeanIt.Web/LeantIt.Web/LeantIt.Web/wwwroot/js/register.js
@@ -5,8 +5,6 @@
     if (cpf.length === 11) {
         cpf = cpf.substring(0, 3) + '.' + cpf.substring(3, 6) + '.' + cpf.substring(6, 9) + '-' + cpf.substring(9);
         cpfInput.value = cpf;
-    } else {
-        alert('CPF inválido');
     }
 }
 function dadosEndereco() {
